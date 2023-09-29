78e47a6ea2bf427869ac64f577de76805fa77da8
Sruthi <sruthivs64@gmail.com>
2023-09-30 00:54:45 +0530

    fixed multidigit and float calculation issue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function input(n)
 {
-    arr=document.getElementById("input").value+=n;
+    document.getElementById("input").value+=n;
 }
 
 function split(){
@@ -8,43 +8,71 @@ function split(){
  var userInput=document.getElementById("input").value;
  var result=[];
  result=userInput.split("");
+ console.log(result);
  operate(result);
  if(result.length=1){
      return result;
  }
 }
 
+function concat(arr,i){
+    console.log(i);
+    while(Number.isInteger(parseFloat(arr[i+1]))==true||arr[i+1]=='.' && (i+1)<arr.length){ 
+    let a=arr[i];//7
+        console.log(a);
+        let b=arr[i+1];
+        console.log(b);
+        console.log("−");
+        arr.splice(i,2);
+        arr.splice(i,0,a+b);
+       console.log(arr);
+    }
+    return arr;    
+
+}
+
+
 function operate(result){
 
  let value;
- for(let i=1;i<result.length;i++){ 
-    if(Number.isInteger(parseInt(result[i]))==true){
-        let a=result[i-1];
-        let b=result[i];
-        result.shift();
-        result.shift();
-        result.unshift(a+b);
-        i--;
+ for(let i=0;i<result.length-1;i++){ 
+    if(Number.isInteger(parseFloat(result[i]))==true){
+        concat(result,i);
+ 
     }
-    if(result[i]=='+'){
+    else if(result[i]=='+'){
+        console.log(i);
+        concat(result,i+1);
+        console.log(result);
         value=sum(result[i-1],result[i+1]);
+        console.log("I'm called");
         reduce(result,value);
     }
-    else if(result[i]=='-'){
+    else if(result[i]=='−'){
+        console.log(i);
+        concat(result,i+1);
+        console.log(result);
         value=subtract(result[i-1],result[i+1]);
         reduce(result,value);
     }
     else if(result[i]=='÷'){
+        console.log(i);
+        concat(result,i+1);
+        console.log(result);
         value=divide(result[i-1],result[i+1]);
         reduce(result,value);
     }
     else if(result[i]=='×'){
+        console.log(i);
+        concat(result,i+1);
+        console.log(result);
         value=multiply(result[i-1],result[i+1]);
 
         reduce(result,value);
     }
   } 
 }
+
 
 function reduce(res,val){
 
@@ -59,19 +87,21 @@ function reduce(res,val){
 
 
 function sum(a,b){
-    return parseInt(a)+parseInt(b);
+    console.log("sum of" + a);
+    console.log("and sum of" + b);
+    return parseFloat(a)+parseFloat(b);
 }
 
 function subtract(a,b){
-   return parseInt(a)-parseInt(b);
+   return parseFloat(a)-parseFloat(b);
 }
 
 function divide(a,b){
-    return parseInt(a)/parseInt(b);
+    return parseFloat(a)/parseFloat(b);
 }
 
 function multiply(a,b){
-    return parseInt(a)*parseInt(b);
+    return parseFloat(a)*parseFloat(b);
 }
 
 function equalTo(){
